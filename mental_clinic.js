604e7cc66c2844e9c1cf32e5ee128b741b1b6647
will <will_definite_choice@yahoo.co.jp>
2020-07-13 18:45:09 +0900

    Create mental_clinic.js

---
+++ mental_clinic.js
@@ -0,0 +1,28 @@
+﻿/*
+var _window = $(window),
+    introBottom;
+ 
+_window.on('scroll',function(){     
+    introBottom = $('.intro').height();
+    console.log(introBottom);
+    if(_window.scrollTop() > introBottom){
+        $('header').addClass('fixed');
+    }
+    else{
+        $('header').removeClass('fixed');   
+    }
+});
+ 
+_window.trigger('scroll');
+*/
+
+/* PC版は電話のリンクを削除する */
+var ua = navigator.userAgent.toLowerCase();
+var isMobile = /iphone/.test(ua)||/android(.+)?mobile/.test(ua);
+
+if (!isMobile) {
+    $(document).on('click', 'a[href^="tel:"]', function(e) {
+        e.preventDefault();
+    });
+}
+
