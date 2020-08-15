1cc9ea7083262b21e2aa571c8c2e3d7c96387a84
will <will_definite_choice@yahoo.co.jp>
2020-08-15 19:58:44 +0900

    paypalボタン配置

--- mental_clinic.php
+++ mental_clinic.php
@@ -148,6 +148,34 @@ header('X-FRAME-OPTIONS: DENY');
         <p class = "time-detail">
           お支払いはPayPalでお願いしております。<br>
           カウンセリング開始時間前までにお支払い下さい。
+          <!-- paypal決済ボタン -->
+          <div id="paypal-button-container"></div>
+          <script src="https://www.paypal.com/sdk/js?client-id=sb&currency=JPY" data-sdk-integration-source="button-factory"></script>
+          <script>
+          paypal.Buttons({
+            style: {
+              shape: 'pill',
+              color: 'blue',
+              layout: 'horizontal',
+              label: 'paypal',
+
+            },
+            createOrder: function(data, actions) {
+              return actions.order.create({
+                purchase_units: [{
+                  amount: {
+                    value: '6000'
+                  }
+                }]
+              });
+            },
+            onApprove: function(data, actions) {
+              return actions.order.capture().then(function(details) {
+                alert('Transaction completed by ' + details.payer.name.given_name + '!');
+              });
+            }
+          }).render('#paypal-button-container');
+        </script>
         </p>
       </div>
       <div class = "time-wrapper">
